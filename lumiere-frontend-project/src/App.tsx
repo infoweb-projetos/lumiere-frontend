53c3fd71ccbc5190778bbad221e9f74b396f0ea9
Pedro Varela <pedrao.varela@gmail.com>
2023-07-13 22:18:45 -0300

    feat: teste protected main

--- lumiere-frontend-project/src/App.tsx
+++ lumiere-frontend-project/src/App.tsx
@@ -3,6 +3,7 @@ import { Horse, Heart, Cube } from '@phosphor-icons/react';
 function App() {
   return (
     <>
+      {/*  */}
       <div className="text-3xl font-bold content-center flex justify-center items-center h-screen">
         <p className="bg-blue-400">
           Tailwind funcionou, Baixe a extensão Tailwind CSS IntelliSense
