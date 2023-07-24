936230d023a9eccf8b9db503bff40cdb56867950
Raquel Garcia <raqueldossantosgarcia@gmail.com>
2023-07-24 00:34:32 -0300

    error: rating stars sem initial value fixed

--- lumiere-frontend-project/src/App.tsx
+++ lumiere-frontend-project/src/App.tsx
@@ -3,6 +3,7 @@ import { MenuNoLogin } from './components/menu/menu-no-login';
 import { DisplayTitulo } from './components/texts/display-sm/titulo';
 import { MontH1 } from './components/texts/monteserrat/h1';
 import { MontP } from './components/texts/monteserrat/p';
+import {CardAmarelo} from './components/cards/card-menu-amarelo';
 
 function App() {
   const a = false;
@@ -14,6 +15,7 @@ function App() {
       <div className="mt-16 flex h-[1000px] flex-col content-center items-center bg-gray-200 p-8 text-3xl font-bold">
         <div className="space-y-8">
           <DisplayTitulo>Melhores Advogados</DisplayTitulo>
+          <CardAmarelo name='Pedro Gustavo' description='Adoro me concentrar em casos complicados! estou aqui para te ajudar e dar fim ao seu caso.' rating="3" photourl="/card-1.svg" ></CardAmarelo>
           <MontH1>Advogados</MontH1>
           <MontP>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi possimus dolor dolorem corporis officiis
