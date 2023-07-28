4aab77a4773fad089a312e4e5967aacc5fad7d47
Pedro Henrique de Sousa Varela <78501819+Pedrohsv1@users.noreply.github.com>
2023-07-28 00:55:30 -0300

    Revert "Merge pull request #20 from infoweb-projetos/15/front"
    This reverts commit e532514c18b64953a635a40ce01771780f5a5a1f, reversing
    changes made to fa139b3cb646460f3a5bbaf68be2f538b3c4088e.

--- lumiere-frontend-project/src/App.tsx
+++ lumiere-frontend-project/src/App.tsx
@@ -1,9 +1,34 @@
-import Cadastro from './pages/cadastro/index';
+import { MenuLogin } from './components/menu/menu-login';
+import { MenuNoLogin } from './components/menu/menu-no-login';
+import { DisplayTitulo } from './components/texts/display-sm/titulo';
+import { MontH1 } from './components/texts/monteserrat/h1';
+import { MontP } from './components/texts/monteserrat/p';
 
 function App() {
+  const a = false;
   return (
     <>
-      <Cadastro />
+      {a ? <MenuNoLogin /> : <MenuLogin />}
+
+      {/*  */}
+      <div className="mt-16 flex h-[1000px] flex-col content-center items-center bg-gray-200 p-8 text-3xl font-bold">
+        <div className="space-y-8">
+          <DisplayTitulo>Melhores Advogados</DisplayTitulo>
+          <MontH1>Advogados</MontH1>
+          <MontP>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi possimus dolor dolorem corporis officiis
+            voluptas saepe odio placeat aliquam, iusto fugit ut, in doloremque dolorum recusandae eaque provident iste
+            a.
+          </MontP>
+          <MontP className=" text-right text-semantic-red">Erro no console</MontP>
+          <MontH1>Lawyers</MontH1>
+          <MontP>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi possimus dolor dolorem corporis officiis
+            voluptas saepe odio placeat aliquam, iusto fugit ut, in doloremque dolorum recusandae eaque provident iste
+            a.
+          </MontP>
+        </div>
+      </div>
     </>
   );
 }
--- lumiere-frontend-project/src/main.tsx
+++ lumiere-frontend-project/src/main.tsx
@@ -1,15 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
-import { QueryClient, QueryClientProvider } from 'react-query';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-const queryClient = new QueryClient();
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <QueryClientProvider client={queryClient}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </QueryClientProvider>,
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 );
