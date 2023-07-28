a4e49d30b459813e7f84b0edfd1cffc2d1222f0e
Pedro Henrique de Sousa Varela <78501819+Pedrohsv1@users.noreply.github.com>
2023-07-28 00:58:07 -0300

    Revert "sda"
    This reverts commit f8b15690eda68cb39eadd6f5d02dce613d6763d8.

--- lumiere-frontend-project/src/App.tsx
+++ lumiere-frontend-project/src/App.tsx
@@ -5,14 +5,32 @@ import { MontH1 } from './components/texts/monteserrat/h1';
 import { MontP } from './components/texts/monteserrat/p';
 
 function App() {
-  return(
-    <Routes>
-    <Route path="/" element={<HomePage/>} />
-    <Route path="/ProcurarAdvogados" element={<ProcurarAdvogados/>}/>
-    <Route path="/ProcurarAdvogados/:id" element={<Individual/>}/>
+  const a = false;
+  return (
+    <>
+      {a ? <MenuNoLogin /> : <MenuLogin />}
 
-  </Routes>
-  )
+      {/*  */}
+      <div className="mt-16 flex h-[1000px] flex-col content-center items-center bg-gray-200 p-8 text-3xl font-bold">
+        <div className="space-y-8">
+          <DisplayTitulo>Melhores Advogados</DisplayTitulo>
+          <MontH1>Advogados</MontH1>
+          <MontP>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi possimus dolor dolorem corporis officiis
+            voluptas saepe odio placeat aliquam, iusto fugit ut, in doloremque dolorum recusandae eaque provident iste
+            a.
+          </MontP>
+          <MontP className=" text-right text-semantic-red">Erro no console</MontP>
+          <MontH1>Lawyers</MontH1>
+          <MontP>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi possimus dolor dolorem corporis officiis
+            voluptas saepe odio placeat aliquam, iusto fugit ut, in doloremque dolorum recusandae eaque provident iste
+            a.
+          </MontP>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
