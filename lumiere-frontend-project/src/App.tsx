1207430bd63cdea3df0b3006c97bff376f277bbc
Raquel Garcia <raqueldossantosgarcia@gmail.com>
2023-10-06 00:15:08 -0300

    feat: erro date calendar

--- lumiere-frontend-project/src/App.tsx
+++ lumiere-frontend-project/src/App.tsx
@@ -1,7 +1,7 @@
 import { Routes, Route} from 'react-router-dom'
 import ProcurarAdvogados from '../src/pages/procurar-advogados/';
 import Individual from '../src/components/cards/individual/';
-import CasoAdvogado from '../src/pages/casos-advogado';
+import CasoAdvogado from '../src/pages/casos-advogado/index.tsx';
 import HomePage from '../src/pages/homepage/';
 import "./index.css";
 import Cadastro from './pages/cadastro';
--- lumiere-frontend-project/src/pages/criar-casos-advogado/index.tsx
+++ lumiere-frontend-project/src/pages/criar-casos-advogado/index.tsx
@@ -1,7 +1,7 @@
 import {DateCalendar} from '@mui/x-date-pickers/DateCalendar'; 
-import * as React from 'react';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import * as React from 'react';
 import { useState } from 'react';
 import * as Yup from 'yup';
 import { ErrorsForm} from './caso.interface';
@@ -11,6 +11,11 @@ import { ApiError, CreateCaseResponse } from '../../api/services/casos/criarCaso
 import { createCase } from '../../api/services/casos';
 import { MenuLogin } from '../../components/menu/menu-login';
 import { MenuNoLogin } from '../../components/menu/menu-no-login';
+import { DisplayH1 } from '../../components/texts/display-sm/h1';
+import { Label } from '../../components/label';
+import { InputText } from '../../components/input/input-text';
+import { MontInfo } from '../../components/texts/monteserrat/info';
+import { InputTextArea } from '../../components/input/input-textarea';
 
 export default function CriarCaso () {
     const [titulo, setTitulo] = useState('');
@@ -82,9 +87,30 @@ export default function CriarCaso () {
       return(
         <>
           {a ? <MenuNoLogin /> : <MenuLogin />}
-          <main className="pt-24 bg-gray-200 pl-16 pr-16 min-h-screen">
-            
-            
+          <main className=" bg-gray-200 p-48 min-h-screen">
+            <div className='bg-white pt-16 pb-16 pl-24 pr-24'>
+            <div className="flex flex-col mb-8">
+              <DisplayH1 className='text-[#4A4A4A] mb-4 text-[40px]'>Procurar advogados</DisplayH1>
+              <label className="font-mont text-[20px] font-semibold">Titulo</label>
+              <InputText className="bg-white "erro={false} placeholder="Ex: Pensão alimentícia" name="titulo" value={titulo} onChange={setTitulo} />
+              <MontInfo className="text-semantic-red">{validationFormError.titulo}</MontInfo>
+            </div>
+            <div className=" flex flex-col mb-8">
+              <label className="font-mont text-[20px] font-semibold">Descrição</label>
+              <InputTextArea className="bg-white h-36" erro={false} placeholder="" name="descricao" value={descricao} onChange={setDescricao} />
+              <MontInfo className="text-semantic-red">{validationFormError.descricao}</MontInfo>
+            </div>
+            <div className=" flex flex-col mb-8">
+              <label className="font-mont text-[20px] font-semibold">Nome do cliente</label>
+              <InputText className="bg-white" erro={false} placeholder="Ex: Maria das Graças" name="nome" value={nomeCliente} onChange={setnomeCliente} />
+              <MontInfo className="text-semantic-red">{validationFormError.nomecliente}</MontInfo>
+            </div>
+            <LocalizationProvider>
+              <DateCalendar />
+            </LocalizationProvider>
+
+            </div>
+
           </main>        
         
         
