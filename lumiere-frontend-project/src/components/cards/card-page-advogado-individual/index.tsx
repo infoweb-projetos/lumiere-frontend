0728246ddc4dfb745eb5e3f77a3a715f04849614
Pedro Henrique de Sousa Varela <78501819+Pedrohsv1@users.noreply.github.com>
2023-10-19 19:06:11 -0300

    refactor: unic lawyer

--- lumiere-frontend-project/src/components/cards/card-page-advogado-individual/index.tsx
+++ lumiere-frontend-project/src/components/cards/card-page-advogado-individual/index.tsx
@@ -1,54 +1,64 @@
 import { MontH3 } from '../../texts/monteserrat/h3';
 import { MontP } from '../../texts/monteserrat/p';
 import { ICardProps } from '../card.interface.ts';
-import { Rating } from 'react-simple-star-rating'
+import { Rating } from 'react-simple-star-rating';
 import { ButtonYellow } from '../../buttons/button-yellow-icon/index.tsx';
 
+export const CardPageAdvogadoIndividual = ({ name, photourl }: ICardProps) => {
+  // const onPointerMove = (value: number, index: number) => value=parseInt(rating);
 
-export const CardPageAdvogadoIndividual = ({ name, photourl} :  ICardProps) =>{
-    // const onPointerMove = (value: number, index: number) => value=parseInt(rating);
-
-    return(
-        <div className='bg-blue-300 flex flex-row pb-4 pt-28 w-full pl-20 pr-20 justify-center'>
-            <div className='flex flex-row max-w-[1500px]'>
-            <div className='flex flex-col w-1/4 items-center'>
-                <div className='flex flex-row justify-center '>
-                    <MontH3 className="text-white mr-4">{name}</MontH3>
-                    <Rating size={35} disableFillHover={true} allowHover={false} fillColor="#D1BC87" SVGstyle={{'display':'inline'}}initialValue={parseInt("4")}/>
-                </div>
-                <img className='-rotate-90 w-80 object-cover' src={photourl}></img>
-            </div>
-            <div className="flex flex-col w-1/4 items-center mr-10">
-                <div>
-                    <div className="pb-8">
-                        <MontH3 className="text-white pb-2">Casos vencidos</MontH3>
-                        <img src="/verde-bola.svg"></img>
-                    </div>
-                    <div className="pb-8">
-                        <MontH3 className="text-white pb-2">Casos perdidos</MontH3>
-                        <img src="/vermelho-bola.svg"></img>
-                    </div>
-                    <div>
-                        <MontH3 className="text-white pb-2">CNPJ</MontH3>
-                        <MontP className="text-white pb-2">12.345.678/0002-00</MontP>
-                    </div>
-                </div>
-            </div>
-            <div className="flex flex-col w-3/6">
-                <div className="pb-8">
-                    <MontH3 className="text-white pb-2">Histórico de atuação</MontH3>
-                    <MontP className="text-white pb-2">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words</MontP>
-                </div>
-                <div className="pb-8">
-                    <MontH3 className="text-white pb-2">Áreas de atuação</MontH3>
-                    <div className="flex">
-                        <MontP className="p-2 bg-white rounded-sm ">FAMILIAR</MontP>
-                        <MontP className="p-2 bg-white rounded-sm ml-5">ROUBOS</MontP>
-                    </div>
-                </div>
-                <ButtonYellow title={"Entrar em contato"} size={"xl"}></ButtonYellow>
-            </div>
-            </div>
+  return (
+    <div className="flex w-full flex-row justify-center bg-blue-300 pb-4 pl-20 pr-20 pt-28">
+      <div className="flex max-w-[1500px] flex-row">
+        <div className="flex w-1/4 flex-col items-center">
+          <div className="flex flex-row justify-center ">
+            <MontH3 className="mr-4 text-white">{name}</MontH3>
+            <Rating
+              size={35}
+              disableFillHover={true}
+              allowHover={false}
+              fillColor="#D1BC87"
+              SVGstyle={{ display: 'inline' }}
+              initialValue={parseInt('4')}
+            />
+          </div>
+          <img className="w-80 -rotate-90 object-cover" src={photourl} alt=''/>
         </div>
-    )
-}
+        <div className="mr-10 flex w-1/4 flex-col items-center">
+          <div>
+            <div className="pb-8">
+              <MontH3 className="pb-2 text-white">Casos vencidos</MontH3>
+              <img src="/verde-bola.svg" alt=''/>
+            </div>
+            <div className="pb-8">
+              <MontH3 className="pb-2 text-white">Casos perdidos</MontH3>
+              <img src="/vermelho-bola.svg" alt=''/>
+            </div>
+            <div>
+              <MontH3 className="pb-2 text-white">CNPJ</MontH3>
+              <MontP className="pb-2 text-white">12.345.678/0002-00</MontP>
+            </div>
+          </div>
+        </div>
+        <div className="flex w-3/6 flex-col">
+          <div className="pb-8">
+            <MontH3 className="pb-2 text-white">Histórico de atuação</MontH3>
+            <MontP className="pb-2 text-white">
+              Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical
+              Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at
+              Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words
+            </MontP>
+          </div>
+          <div className="pb-8">
+            <MontH3 className="pb-2 text-white">Áreas de atuação</MontH3>
+            <div className="flex">
+              <MontP className="rounded-sm bg-white p-2 ">FAMILIAR</MontP>
+              <MontP className="ml-5 rounded-sm bg-white p-2">ROUBOS</MontP>
+            </div>
+          </div>
+          <ButtonYellow title={'Entrar em contato'} size={'xl'}></ButtonYellow>
+        </div>
+      </div>
+    </div>
+  );
+};
--- lumiere-frontend-project/src/pages/advogado-individual/advogado-individual.tsx
+++ lumiere-frontend-project/src/pages/advogado-individual/advogado-individual.tsx
@@ -1,66 +1,52 @@
-import  { useState, useEffect } from 'react';
 import { MontH2 } from '../../components/texts/monteserrat/h2';
 import { MenuLogin } from '../../components/menu/menu-login';
 import { MenuNoLogin } from '../../components/menu/menu-no-login';
 import { Footer } from '../../components/footer';
 import { CardPageAdvogadoIndividual } from '../../components/cards/card-page-advogado-individual';
 import { Comentario } from '../../components/cards/comentario';
-import axiosInstance from '../../api/axiosinstance';
 import { useQuery } from 'react-query';
+import { GetLawyer } from '../../api/services/advogados/get-lawyers';
+import { useParams } from 'react-router-dom';
 
-interface PropsAdv {
-  id: number | null;
-}
-interface PropsAdvsList {
-  id: number;
-  nome: string;
-  email: string;
-  cnpj: string;
-  historico: string;
-  areaDeAtuacao: string;
-}
-
-export const AdvogadoIndividual = ({ id }: PropsAdv) => {
-  async function loadAdvs() {
-    const response = await axiosInstance.get('/advogado');
-    return response.data
-  }
-  
+export const AdvogadoIndividual = () => {
   const a = true;
-  
-  const advs = useQuery("advs", loadAdvs)
+  const { id } = useParams();
+  const advs = useQuery('advs', GetLawyer);
 
   return (
     <>
       {a ? <MenuNoLogin /> : <MenuLogin />}
       <div className="flex flex-row">
-        {advs.isFetched && advs.data.map((adv: PropsAdvsList, index: number) => {
-          if (id == index) {
-            return (
-              <CardPageAdvogadoIndividual
-                key={id}
-                referencia={'null'}
-                name={adv.nome}
-                description={'Advogado muito top'}
-                rating={'4'}
-                photourl={'/elizia-advogada.svg'}
-              />
-            );
-          }
-        })}
+        {advs.isFetched &&
+          id &&
+          advs.data &&
+          advs.data.map((adv) => {
+            if (id == adv.id) {
+              return (
+                <CardPageAdvogadoIndividual
+                  key={id}
+                  referencia={'null'}
+                  name={adv.nome}
+                  description={'Advogado muito top'}
+                  rating={'4'}
+                  photourl={'/elizia-advogada.svg'}
+                />
+              );
+            }
+          })}
       </div>
-      <div className="bg-gray-200 pl-20 pr-20 pt-8 flex flex-col items-center">
-      <div className='flex flex-col max-w-[1500px] w-full mb-8'>
-        <MontH2 className="mb-10 text-3xl">Avaliações</MontH2>
-        <div className="bg-white">
-          <Comentario
-            referencia={'null'}
-            name={'Raquel Garcia'}
-            description={'Advogado muito top'}
-            rating={'4'}
-            photourl={'/comentario-exemplo.svg'}
-          />
-        </div>
+      <div className="flex flex-col items-center bg-gray-200 pl-20 pr-20 pt-8">
+        <div className="mb-8 flex w-full max-w-[1500px] flex-col">
+          <MontH2 className="mb-10 text-3xl">Avaliações</MontH2>
+          <div className="bg-white">
+            <Comentario
+              referencia={'null'}
+              name={'Raquel Garcia'}
+              description={'Advogado muito top'}
+              rating={'4'}
+              photourl={'/comentario-exemplo.svg'}
+            />
+          </div>
         </div>
       </div>
       <Footer />
